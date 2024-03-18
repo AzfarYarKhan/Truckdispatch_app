cc1e42b0d8785a4571552c5236c95ab0ba31f741
azfar khan <azfaryar004@gmail.com>
2024-03-19 02:22:43 +0500

    Cron Job update_job_status

---
+++ app/api/job/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/app/libs/prismadb";
+import { NextRequest, NextResponse } from "next/server";
+import { verifySignatureAppRouter } from "@upstash/qstash/dist/nextjs";
+
+export async function UpdateJobs_status(request: NextRequest) {
+  try {
+    console.log("end point hit");
+    const jobs = await prisma.job.findMany();
+    for (const job of jobs) {
+      if (job.status !== "COMPLETED" && job.status !== "CANCELLED") {
+        const today = new Date();
+        if (job.date < today) {
+          await prisma.job.update({
+            where: { id: job.id },
+            data: { status: "MISSED" },
+          });
+        }
+      }
+    }
+    return NextResponse.json({ message: "Jobs statuses Updated Successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error Updating Job status" });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export const POST = verifySignatureAppRouter(UpdateJobs_status);
