cd66e94044401c877dc00dafae919fcb51729404
azfar khan <azfaryar004@gmail.com>
2024-05-24 01:19:59 +0500

    Stripe API key fix for prod

--- app/libs/stripe.ts
+++ app/libs/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.Stripe_API_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   apiVersion: "2024-04-10",
   typescript: true,
 });
