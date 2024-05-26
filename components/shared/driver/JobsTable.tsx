c4d7125115ee4db5ab474557be48194d564b46f0
azfar khan <azfaryar004@gmail.com>
2024-05-26 18:32:20 +0500

    Driver responsiveness fix jobs page

--- components/shared/driver/JobsTable.tsx
+++ components/shared/driver/JobsTable.tsx
@@ -22,7 +22,7 @@ export default function JobsTable({ driverId }: { driverId: string }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex flex-col pt-20 px-1 ">
+      <div className="flex flex-col pt-10 px-1 ">
         <div className="container mx-auto py-5 md:px-10 lg:px-32 mt-2">
           <div className="flex flex-col gap-4 mx-auto pb-2 max-w-3xl">
             <DataTable columns={columns} data={data} />
