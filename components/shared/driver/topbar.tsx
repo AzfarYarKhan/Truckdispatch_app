4de4822b2b14609dd626c834dc4cf926b8d9cbfa
azfar khan <azfaryar004@gmail.com>
2024-05-26 18:26:42 +0500

    Logo removed driver topbar

--- components/shared/driver/topbar.tsx
+++ components/shared/driver/topbar.tsx
@@ -11,7 +11,7 @@ function Topbar() {
         href="/driver/jobs"
         className="flex items-center gap-4 p-3 max-sm:p-1"
       >
-        <Image src="/vercel.svg" alt="logo" width={60} height={70} />
+        {/* <Image src="/vercel.svg" alt="logo" width={60} height={70} /> */}
       </Link>
     </nav>
   );
